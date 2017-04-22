480277b9c367290b41d5d87a60adae518c7359e9
Robert Harężlak <harezlak.robert7@gmail.com>
2017-04-23 01:15:36 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422225629) do
+ActiveRecord::Schema.define(version: 20170422230320) do
+
+  create_table "points_of_interests", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_points_of_interests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
