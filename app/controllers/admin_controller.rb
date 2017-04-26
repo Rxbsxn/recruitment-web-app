915863e32681e9ffcf6b72639ca209f0c8055baa
Robert Harężlak <harezlak.robert7@gmail.com>
2017-04-26 16:39:52 +0200

    admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,14 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
+  before_filter :verify_is_admin
 
   def index
-    @users = User.all
+    @users = User.all.decorate
+  end
+
+  private
+
+  def verify_is_admin
+    (current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
   end
 end
