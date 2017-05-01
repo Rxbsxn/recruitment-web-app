1e16f31f301f2234c7d9199a1931418ac0ef6b7a
Robert Harężlak <harezlak.robert7@gmail.com>
2017-05-01 14:38:10 +0200

    points of interests validation

--- app/models/points_of_interest.rb
+++ app/models/points_of_interest.rb
@@ -1,3 +1,8 @@
 class PointsOfInterest < ApplicationRecord
+  TYPES = ['health', 'hobby', 'work'].freeze
+  
   belongs_to :user
+
+  validates :type , inclusion: { in: TYPES }
+  validates :points, numericality: { only_integer: true }
 end
