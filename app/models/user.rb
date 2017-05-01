ac39709faa23567d8772fd14d45e92123b2e64ab
Robert Harężlak <harezlak.robert7@gmail.com>
2017-05-01 14:35:04 +0200

    allow nested attribs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :points_of_interests
 
+  accepts_nested_attributes_for :points_of_interests, reject_if: :all_blank, allow_destroy: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
