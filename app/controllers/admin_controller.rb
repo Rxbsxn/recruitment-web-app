cd4d49aa324e277cb4b11f91828bbdb6fa21b296
Robert Harężlak <harezlak.robert7@gmail.com>
2017-05-01 14:43:20 +0200

    prepare controller for new method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,8 +5,26 @@ class AdminController < ApplicationController
   def index
     @users = User.all.decorate
   end
+  
+  def new 
+    @user = User.new
+    @user.points_of_interests.new
+  end
+
+  def create
+    @user = User.default_password(user_params)
+    if @user.save
+      redirect_to admin_index_path, notice: 'Done'
+    else
+      render :new
+    end 
+  end
 
   private
+  
+  def user
+    @user = User.find(params[:id])
+  end
 
   def verify_is_admin
     if current_user.nil?
@@ -16,5 +34,19 @@ class AdminController < ApplicationController
         redirect_to(root_path)
       end
     end
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :gender,
+      :age,
+      points_of_interests_attributes: [
+        :id,
+        :name,
+        :type,
+        :points
+      ]
+    )
   end
 end
